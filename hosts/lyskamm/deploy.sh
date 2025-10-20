94239cc48c2279462c9fce07d9a1347636205af2
Igor <igor@sgf.lt>
2025-10-20 22:30:22 +0300

    Use transmission

--- hosts/lyskamm/deploy.sh
+++ hosts/lyskamm/deploy.sh
@@ -3,16 +3,16 @@
 # This script manages deployed containers on the server using quadlets.
 # It should be idempotent and aims to be as declarative as possible.
 
-rsync -rvt --delete ./quadlets/* root@192.168.1.187:/etc/containers/systemd/
+rsync -rvt --delete ./quadlets/* root@192.168.1.184:/etc/containers/systemd/
 
-ssh root@192.168.1.187 << 'EOF'
+ssh root@192.168.1.184 << 'EOF'
 
 systemctl daemon-reload
 
 systemctl restart \
   caddy.service \
   gluetun.service \
-  qbittorrent.service \
+  transmission.service \
   sonarr.service \
   prowlarr.service \
   jellyfin.service
